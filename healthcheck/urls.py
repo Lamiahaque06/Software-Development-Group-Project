29719c88c5d4adfe6c1b7c5ca0a5649981a2b106
lamia <w1972138@westminster.ac.uk>
2025-04-22 10:52:10 +0500

    base file

---
+++ healthcheck/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('apps.authentication.urls')),
+    path('vote/', include('apps.voting.urls')),
+    path('session/', include('apps.sessions.urls')),
+    path('viz/', include('apps.visualization.urls')),
+    path('control/', include('apps.admincontrols.urls')),
+]
