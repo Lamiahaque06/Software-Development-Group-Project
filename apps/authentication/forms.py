f61b425719008bf5c82f6ccd3530099dae368239
ezdasli <132148722+ezdasli@users.noreply.github.com>
2025-04-29 11:24:02 +0100

    Create Forms

---
+++ apps/authentication/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'role')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email', 'username']
+
+class EngineerSignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'engineer'
+        if commit:
+            user.save()
+        return user
+
+class TeamLeaderSignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'team_lead'
+        if commit:
+            user.save()
+        return user
+
+class DepartmentLeaderSignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'dept_lead'
+        if commit:
+            user.save()
+        return user
+
+class SeniorManagerSignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'senior_manager'
+        if commit:
+            user.save()
+        return user
