0db3ea32f3c0953d9cec8bcbf947918eaa155fbb
Zeynep <164501016+TYTMR2@users.noreply.github.com>
2025-04-30 18:56:15 +0100

    Finalizing Process

--- apps/visualization/DjangoAssignment/djapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Team, Question, Response, Department
-
-admin.site.register(Team)
-admin.site.register(Question)
-admin.site.register(Response)
-admin.site.register(Department)
