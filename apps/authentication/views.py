23fbc3bac4b8c973ba61f1003dc71722306db6d7
lamia <w1972138@westminster.ac.uk>
2025-04-30 12:18:25 +0500

    Revert "Merge pull request #4 from Lamiahaque06/damla"
    This reverts commit 879e33ac825b5724873d55a4ba485ab5175daf79, reversing
    changes made to 2850b8e79d778b0e66616cb47986b17655b7db61.

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,59 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import (
-    CustomUserCreationForm, CustomUserChangeForm,
-    EngineerSignUpForm, TeamLeaderSignUpForm,
-    DepartmentLeaderSignUpForm, SeniorManagerSignUpForm,
-    UserUpdateForm
-)
-from django.contrib import messages
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-def engineer_signup(request):
+def register_view(request):
     if request.method == 'POST':
-        form = EngineerSignUpForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('profile')
     else:
-        form = EngineerSignUpForm()
-    return render(request, 'accounts/engineer_signup.html', {'form': form})
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
 
-def team_leader_signup(request):
-    if request.method == 'POST':
-        form = TeamLeaderSignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('profile')
-    else:
-        form = TeamLeaderSignUpForm()
-    return render(request, 'accounts/team_leader_signup.html', {'form': form})
-
-def department_leader_signup(request):
-    if request.method == 'POST':
-        form = DepartmentLeaderSignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('profile')
-    else:
-        form = DepartmentLeaderSignUpForm()
-    return render(request, 'accounts/department_leader_signup.html', {'form': form})
-
-def senior_manager_signup(request):
-    if request.method == 'POST':
-        form = SeniorManagerSignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('profile')
-    else:
-        form = SeniorManagerSignUpForm()
-    return render(request, 'accounts/senior_manager_signup.html', {'form': form})
-
-# Login / Logout / Profile remain same as you have, small fix below:
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -72,16 +32,11 @@ def logout_view(request):
 
 @login_required
 def profile_view(request):
-    return render(request, 'accounts/profile.html')
-
-@login_required
-def profile_update(request):
     if request.method == 'POST':
-        form = UserUpdateForm(request.POST, instance=request.user)
+        form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully.')
             return redirect('profile')
     else:
-        form = UserUpdateForm(instance=request.user)
-    return render(request, 'accounts/profile_update.html', {'form': form})
+        form = CustomUserChangeForm(instance=request.user)
+    return render(request, 'accounts/profile.html', {'form': form})
