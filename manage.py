921acdddf42a1a442cc2ab9c269e5eedac1d0918
lamia <w1972138@westminster.ac.uk>
2025-04-30 17:49:05 +0500

    corrected base and html

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcheck.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'healthcheck.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
